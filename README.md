8d8cd9f7bea77c0c131a105c3720ed85d80ccb1f
AlwaysGuilty <tv6864@student.uni-lj.si>
2023-01-30 01:37:26 +0100

    mod loader invariant, modified readme

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # ModDownloader
 
-ModDownloader is a Python 3 script that downloads mods for a specific Minecraft version for Fabric mod loader from Modrinth/CurseForge/Github into a specified directory. These parameters, along with path to your mod list file can be set inside the script.
-
 This tool was initially made for personal use, since I use specific Fabric mods for Minecraft, but feel free to modify and use it for yourself.
 
-For the future, I will probably make it mod loader invariant.
+## TODO
+
+* Implement fuzzy searching instead of searching for exact matches
 
 ## Instructions
 
-Adjust `mc_version`, `mod_list` and `mod_dir` variables in the [get_mods.py](get_mods.py) and run the script. Make sure you have Python 3 installed.
+Adjust `mc_version`, `mod_list`, `mod_dir` and `mod_loader` variables in the [get_mods.py](get_mods.py) and run the script. Make sure you have Python 3 installed. You will also need to provide CurseForge API key and e-mail. You can get the key by registering on https://console.curseforge.com/ and requesting it there. For the e-mail, contact me on Discord.
 
 ## Why the hardcoded Github entries?
 
@@ -16,4 +16,4 @@ A couple of mods I use are only available on GitHub, which does provide an API, 
 
 ## Why didn't you just make a modpack?
 
-I don't know, gonna have to look into it.
+I'd still probably have to manually update the modpack for every version.
--- get_mods.py
+++ get_mods.py
@@ -5,21 +5,33 @@ import os
 import requests
 import shutil
 import dotenv
+from enum import Enum
+
+
+# supported mod loaders are intersection of those supported by both platforms
+# format: (CurseForge enum, Modrinth name)
+class ModLoader(Enum):
+    FORGE = (1, "forge")
+    LITELOADER = (3, "liteLoader")
+    FABRIC = (4, "fabric")
+    QUILT = (5, "quilt")
 
 
 """
-Change only these:
+When running the cript, change only these:
 """
 mc_version = "1.19.3"
 mod_list = "mod_list.txt"
 #mod_dir = "C:\Users\%username%\AppData\Roaming\.minecraft\mods"
-mod_dir = f"D:\Minecraft\MultiMC\instances\{mc_version}\.minecraft\mods"
+mod_dir = f"D:\Minecraft\MultiMC\instances\\test\.minecraft\mods"
+mod_loader = ModLoader.FABRIC.value
 
 
 # load environ vars from .env file
 dotenv.load_dotenv()
 CURSEFORGE_API_KEY = os.getenv("CURSEFORGE_API_KEY")
 EMAIL = os.getenv("EMAIL")
+
 
 modrinth_api_url = "https://api.modrinth.com/v2"
 curseforge_api_url = "https://api.curseforge.com"
@@ -41,7 +53,7 @@ def clean_mods_dir():
             else:
                 os.remove(path)        
         except Exception as e:
-            print(f"Failed to remove a mod: {f}, reason: {e}")
+            print(f"Failed to remove {f}, reason: {e}")
             sys.exit(1)
 
 
@@ -67,7 +79,7 @@ def get_from_modrinth(mod: str) -> tuple:
     }
     # params = {
     #     "query": mod,
-    #     "facets": "[[\"categories:fabric\"], [\"versions:" + mc_version + "\"], [\"project_type:mod\"]]"
+    #     "facets": "[['categories:fabric'], ['versions:" + mc_version + "'], ['project_type:mod']]"
     # }
 
     # res = requests.get(modrinth_api_url + "/search", headers=headers, params=params)
@@ -80,10 +92,11 @@ def get_from_modrinth(mod: str) -> tuple:
     #         mod_found = True
     # print(f"{ticker} Found {mod} on Modrinth!")
     params = {
-        "loaders": "[\"fabric\"]",
+        "loaders": "['" + mod_loader[1] + "']",
         "game_versions": "[\"" + mc_version + "\"]"
     }
-    res = requests.get(modrinth_api_url + "/project/" + mod + "/version", headers=headers, params=params)
+    url = modrinth_api_url + "/project/" + mod + "/version"
+    res = requests.get(url, headers=headers, params=params)
     if res.status_code != 200:
         log(f"Failed to find {mod} on Modrinth")
         return None
@@ -102,16 +115,17 @@ def get_from_curseforge(mod: str) -> tuple:
     params = {
         "gameId": 432,
         "gameVersion": mc_version,
-        "modLoaderType": 4,
+        "modLoaderType": mod_loader[0],
         "slug": mod
     }
-    res = requests.get(curseforge_api_url + "/v1/mods/search", params=params, headers=headers)
+    url = curseforge_api_url + "/v1/mods/search"
+    res = requests.get(url, params=params, headers=headers)
     if res.status_code != 200:
         log(f"Failed to find {mod} on CurseForge")
         return None
     
+    # iterate through results to find the correct mod, only searches for an exact match
     res = res.json()
-    # iterate through results to find the correct mod
     mod_found = False
     mod_idx = 0
     for i, result in enumerate(res["data"]):
@@ -126,12 +140,13 @@ def get_from_curseforge(mod: str) -> tuple:
     # extract modId, fileId and filename
     modId = res["data"][mod_idx]["id"]
     files = res["data"][mod_idx]["latestFilesIndexes"]
-    candidates = [x for x in files if x["gameVersion"] == mc_version and x["modLoader"] == 4]
+    candidates = [x for x in files if x["gameVersion"] == mc_version and x["modLoader"] == mod_loader[0]]
     fileId = candidates[mod_idx]["fileId"]
     filename = candidates[mod_idx]["filename"]
 
-    # get download link from curseforge api
-    res = requests.get(curseforge_api_url + f"/v1/mods/{modId}/files/{fileId}", headers=headers)
+    # get download URL from curseforge api
+    url = curseforge_api_url + f"/v1/mods/{modId}/files/{fileId}"
+    res = requests.get(url, headers=headers)
     if res.status_code != 200:
         log(f"Failed to fetch download URL for {mod} from CurseForge API")
         return None
@@ -140,6 +155,7 @@ def get_from_curseforge(mod: str) -> tuple:
 
 
 # ugly, i know
+# note: sleepbackground causes game to hang when creating a new world in 1.19.2 and 1.19.3. Therefore it's recommended to use DynamicFPS (slug: dynamic-fps) instead
 def get_from_github(mod:str) -> tuple:
     github_mods = ["biome-thread-local-fix", "force-port", "sleep-background"]
     github_mods_links = ["https://github.com/RedLime/BiomeThreadLocalFix/releases/download/1.3/BiomeThreadLocalFix-1.3.jar", "https://github.com/DuncanRuns/Force-Port-Mod/releases/download/v1.1.0/forceport-1.1.0.jar", "https://github.com/RedLime/SleepBackground/releases/download/3.8/sleepbackground-3.8-1.15.x-1.19.x.jar"]
